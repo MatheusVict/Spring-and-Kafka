758edd2de064dcd3280b190c324867d1e61baf45
MatheusVict <matheusvictorhenrique@gmail.com>
2024-02-09 11:46:29 -0300

    feat: inital commit
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/main/java/io/matheusvictor/springmessagingwithkafka/SpringMessagingWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package io.matheusvictor.springmessagingwithkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessagingWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMessagingWithKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/matheusvictor/springmessagingwithkafka/SpringMessagingWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.matheusvictor.springmessagingwithkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMessagingWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
